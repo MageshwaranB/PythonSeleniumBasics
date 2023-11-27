0555dfc9914876d8a2ce0281e50fde92847b7ecf
Mageshwaran <mageshbalraj@gmail.com>
2023-11-27 19:06:03 +0530

    adding selenium test with fixtures

--- src/pytestconcepts/conftest.py
+++ src/pytestconcepts/conftest.py
@@ -5,24 +5,49 @@ from selenium import webdriver
 
 browser = "chrome"
 
+"""
+If we want to accept input from the command line, then we have to make use of one pytest function called pytest_addoption(parser),
+Once we have to function where it takes the value from the command line, we have to pass that value to the function
+this is where we are using the request in the getBrowser function
+"""
+
+
+def pytest_addoption(parser):
+    """
+    Make use of the parser function in which we pass the cli option (--browser), what action we have to perform (store),
+    and default value (chrome)
+    :param parser:
+    :return:
+    """
+    parser.addoption("--browser",action="store",default="chrome",help="Please enter the browser name")
+
 
 @pytest.fixture
-def getBrowser():
-    return browser
+def getBrowser(request):
+    _browser=request.config.getoption("--browser") #getting the option
+    return _browser
+
+
+"""Let's say we want to make use of the driver variable inside a class, it is not possible at class level Data 
+members of a class are declared protected by adding a single underscore ‘_’ symbol before the data member of that 
+class.
+We're making use of the request to make a variable at a class level
+request.cls.driver - means we are making the driver variable at class level
+"""
 
 
 @pytest.fixture
-def getDriver(self, getBrowser):
-    driver = None
+def getDriver(request, getBrowser):
+    _driver = None
     if getBrowser == "chrome":
-        driver = webdriver.Chrome()
+        _driver = webdriver.Chrome()
     elif getBrowser == "firefox":
-        driver = webdriver.Firefox()
+        _driver = webdriver.Firefox()
     elif getBrowser == "edge":
-        driver = webdriver.Edge()
-    driver.get("https://www.saucedemo.com/")
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+        _driver = webdriver.Edge()
+    _driver.get("https://www.saucedemo.com/")
+    _driver.maximize_window()
+    _driver.implicitly_wait(10)
 
     """A function's execution can be temporarily halted by using the yield statement, which then returns a value 
     to the caller while saving the state of the function for later resumption.
@@ -30,6 +55,7 @@ def getDriver(self, getBrowser):
     execute the lines of code in getDriver but doesn't execute the quit method, the quit method only get execute the
     when the calling function is done
     """
-    yield driver
+    request.cls.driver = _driver
+    yield request.cls.driver
     time.sleep(2)
-    driver.quit()
+    request.cls.driver.quit()
--- src/pytestconcepts/test_seleniumfixtures.py
+++ src/pytestconcepts/test_seleniumfixtures.py
@@ -12,7 +12,12 @@ from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import TimeoutException
 
 
-
+"""
+Instead of passing the fixture function name at function level, we can pass them at class level with the help of 
+use-fixtures. We can even further reduce this by creating a separate class that holds this use fixture and an another
+class can inherit from that
+"""
+@pytest.mark.usefixtures("getDriver")
 class TestSauceDemo:
     """
     To avoid repetitive lines of code like setting up the driver, we can make use of fixture which will trigger
@@ -21,13 +26,14 @@ class TestSauceDemo:
     modules, then we have to keep this setup in a confttest module
     """
 
+    def test_login(self):
+        driver = self.driver
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
 
-    def test_login(self,getDriver):
-        getDriver.find_element(By.ID,"user-name").send_keys("standard_user")
-        getDriver.find_element(By.ID,"password").send_keys("secret_sauce")
-        getDriver.find_element(By.ID,"login-button").click()
-
-    def test_add_product(self,getDriver):
-        self.test_login(getDriver)#using this test method for logining into the website
-        getDriver.find_element(By.XPATH,"//button[@id='add-to-cart-sauce-labs-backpack']").click()
-        getDriver.find_element(By.XPATH,"//div[@id='shopping_cart_container']/a").click()
+    def test_add_product(self):
+        driver=self.driver
+        self.test_login()  # using this test method for logining into the website
+        driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']").click()
+        driver.find_element(By.XPATH, "//div[@id='shopping_cart_container']/a").click()
